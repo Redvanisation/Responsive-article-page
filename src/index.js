ac8e766eb03764baee5fc9991f0aebcf42e9523e
Redvan <redvanisation@gmail.com>
2020-05-16 18:46:45 -0400

    Add modal closing funtionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './styles/main.scss';
+
+const modal = document.querySelector('#modal');
+const modalCloseX = document.querySelector('#modal-close-x');
+const modalBox = document.querySelector('#modal-box');
+const label = document.querySelector('#modal-box-label');
+const btn = document.querySelector('#modal-box-btn');
+
+const hideModal = (e) => {
+  if (e.target === modal || e.target === modalCloseX) {
+    modal.classList.add('hidden');
+  }
+}
+
+
+modal.addEventListener('click', (e) => hideModal(e))
