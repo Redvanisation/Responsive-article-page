b7278039b657a7e8752cc1a800d6b7f4ad6c3490
Redvan <redvanisation@gmail.com>
2020-05-16 22:15:10 -0400

    Add comments and update README.md

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# IPS-frontend-assignment
-Frontend assignment for IPS
+# Responsive blog page
+
+- Blog page made with **vanilla JavaScript**, **HTML5**, and **Sass**.
+- The live version of this page can be found here [https://blissful-archimedes-b0c960.netlify.app/](https://blissful-archimedes-b0c960.netlify.app/).
+
--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
+// Importing the styles to be bundeled with webpack in main.js
 import './styles/main.scss';
 
+// Selecting the needed nodes
 const modal = document.querySelector('#modal');
 const modalCloseX = document.querySelector('#modal-close-x');
-const modalBox = document.querySelector('#modal-box');
 const input = document.querySelector('#modal-input');
 const label = document.querySelector('#modal-box-label');
 const btn = document.querySelector('#modal-box-btn');
-const articleImage = document.querySelector('#article-img');
 
+
+// Function to hide the modal after a click
 const hideModal = (e) => {
   if (e.target === modal || e.target === modalCloseX) {
     modal.classList.add('hidden');
   }
 }
 
+// Function to revert the input form's styles if email is correct
 const handleCorrectEmail = () => {
   input.classList.remove('wrong-input');
   label.classList.remove('wrong-input');
@@ -22,6 +25,7 @@ const handleCorrectEmail = () => {
   label.textContent = 'Please enter your email here';
 }
 
+// Function to handle the input form's styles if email is incorrect
 const handleIncorrectEmail = () => {
   input.classList.add('wrong-input');
   label.classList.add('wrong-input')
@@ -31,6 +35,7 @@ const handleIncorrectEmail = () => {
   label.textContent = 'Please enter a valid email address';
 }
 
+// Function to validate the email inputed by the user and call the correct function depending on the result
 const validateEmail = () => {
   const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
@@ -43,13 +48,14 @@ const validateEmail = () => {
   }
 }
 
+// Function to revert the input field's styles on blur if there was nothing entered by the use 
 const handleInputBlur = () => {
   if (!input.value) {
     handleCorrectEmail();
   }
 }
 
-
+// Event listeners
 modal.addEventListener('click', (e) => hideModal(e));
 btn.addEventListener('click', () => validateEmail());
 input.addEventListener('blur', () => handleInputBlur())
